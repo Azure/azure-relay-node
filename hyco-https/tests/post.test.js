ab0508c50409472ddd95976e5a60bcfb02f67207
Bainian Liu <bainian12345@hotmail.com>
2019-09-06 21:10:45 -0400

    Fix flaky tests caused by beginning tests before listener opens and added additional tests for POST (#38)

--- hyco-https/tests/post.test.js
+++ hyco-https/tests/post.test.js
@@ -10,18 +10,29 @@ expect(path).toBeDefined();
 expect(keyrule).toBeDefined();
 expect(key).toBeDefined();
 
-// create a large message over 64kb to force a rendezvous connection
-var largeMessage = "";
-for (var i = 1024 * 64; i >= 0; i--) {
-    largeMessage += String.fromCharCode(i % 128);
+var smallMessage = "SmallMessage";
+var exactly64kbMessage = "";
+for (var i = 1024 * 64; i > 0; i--) {
+    exactly64kbMessage += String.fromCharCode(i % 128);
 }
+var over64kbMessage = exactly64kbMessage + String.fromCharCode(0);
 
-function sendAndReceive(requestMsg, responseMsg, done) {
-    var totalRequests = 10; // Total requests to send over the test
-    jest.setTimeout(5000 + (totalRequests * 200)); // Expect 5 seconds + 5 requests per second
+// reqWriteMsgs, reqEndMsg, resWriteMsgs, resEndMsg should be string[], string, or null
+function sendAndReceive(reqWriteMsgs, reqEndMsg, resWriteMsgs, resEndMsg, done) {
+    var reqExpected = "";
+    var resExpected = "";
 
-    var listenerCount = 0;
-    var senderCount = 0;
+    if (reqWriteMsgs) {
+        reqExpected = Array.isArray(reqWriteMsgs) ? reqWriteMsgs.reduce((total, current) => { return total + current; }) : reqWriteMsgs;
+    }
+    reqExpected += (reqEndMsg) ? reqEndMsg : "";
+
+    if (resWriteMsgs) {
+        resExpected = Array.isArray(resWriteMsgs) ? resWriteMsgs.reduce((total, current) => { return total + current; }) : resWriteMsgs;
+    }
+    resExpected += (resEndMsg) ? resEndMsg : "";
+
+    jest.setTimeout(5000); // 5 seconds timeout per test
 
     /* set up the listener */
     var uri = https.createRelayListenUri(ns, path);
@@ -34,11 +45,25 @@ function sendAndReceive(requestMsg, responseMsg, done) {
             expect(req.headers.custom).toBe("Hello");
             req.setEncoding('utf-8');
             req.on('data', (chunk) => {
-                expect(chunk).toBe(requestMsg);
+                expect(chunk.length).toBe(Buffer.byteLength(reqExpected));
+                expect(chunk).toBe(reqExpected);
             });
             req.on('end', () => {
-                res.end(responseMsg);
-                listenerCount++;
+                if (resWriteMsgs) {
+                    if (Array.isArray(resWriteMsgs)) {
+                        resWriteMsgs.forEach((msg) => {
+                            res.write(msg);
+                        });
+                    } else {
+                        res.write(resWriteMsgs);
+                    }
+                }
+
+                if (resEndMsg && resEndMsg.length) {
+                    res.end(resEndMsg);
+                } else {
+                    res.end();
+                }
             });
         });
 
@@ -55,7 +80,7 @@ function sendAndReceive(requestMsg, responseMsg, done) {
     var clientUri = https.createRelayHttpsUri(ns, path);
     var token = https.createRelayToken(clientUri, keyrule, key);
     
-    for (var i = 0; i < totalRequests; i++) {
+    server.on('listening', () => {
         var req = https.request({
             hostname: ns,
             path: ((!path || path.length == 0 || path[0] !== '/') ? '/' : '') + path,
@@ -65,7 +90,7 @@ function sendAndReceive(requestMsg, responseMsg, done) {
                 'ServiceBusAuthorization': token,
                 'Custom' : 'Hello',
                 'Content-Type': 'text/plain',
-                'Content-Length': Buffer.byteLength(requestMsg)
+                'Content-Length': Buffer.byteLength(reqExpected)
             }
         }, (res) => {
             var chunks = '';
@@ -75,40 +100,117 @@ function sendAndReceive(requestMsg, responseMsg, done) {
                 chunks += chunk;
             });
             res.on('end', () => {
-                expect(chunks.length).toBe(responseMsg.length);
-                expect(chunks).toBe(responseMsg);
-                senderCount++;
-                if (listenerCount == totalRequests && senderCount == totalRequests) {
-                    server.close();
-                    jest.clearAllTimers();
-                    done();
-                }
+                expect(chunks.length).toBe(Buffer.byteLength(resExpected));
+                expect(chunks).toBe(resExpected);
+                server.close();
+                jest.clearAllTimers();
+                done();
             });
         }).on('error', (e) => {
             expect(e).toBeUndefined();
         });
 
-        req.write(requestMsg);
-        req.end();
-    }
+        if (reqWriteMsgs) {
+            if (Array.isArray(reqWriteMsgs)) {
+                reqWriteMsgs.forEach((msg) => {
+                    req.write(msg);
+                });
+            } else {
+                req.write(reqWriteMsgs);
+            }
+        }
+
+        if (reqEndMsg && reqEndMsg.length) {
+            req.end(reqEndMsg);
+        } else {
+            req.end();
+        }
+    });
 }
 
-test('HttpEmptyPostReqEmptyRes', (done) => {
-    sendAndReceive("", "", done);
-})
+var testMessages = {
+    // testName : testMessage
+    "SmallMessage" : smallMessage,
+    "Exactly64kbMessage" : exactly64kbMessage,
+    "Over64kbMessage" : over64kbMessage
+}
 
-test('HttpPostSmallReqSmallRes', (done) => {
-    sendAndReceive("Hello", "Goodbye", done);
+describe('HttpPostEmptyReqEmptyResTest', () => {
+    test('HttpPostEmptyReqEmptyRes', (done) => {
+        sendAndReceive(null, "", null, "", done);
+    });
 });
 
-test('HttpPostSmallReqLargeRes', (done) => {
-    sendAndReceive("Hello", largeMessage, done);
+describe('HttpPostRequestTests', () => {
+    describe('EndOnly', () => {
+        Object.keys(testMessages).forEach((testName) => {
+            test('HttpPostRequestEndOnly' + testName, (done) => {
+                sendAndReceive(null, testMessages[testName], null, "ResponseMessage", done);
+            });
+        });
+    });
+    describe('WriteOnly', () => {
+        Object.keys(testMessages).forEach((testName) => {
+            test('HttpPostRequestWriteOnly' + testName, (done) => {
+                sendAndReceive(testMessages[testName], null, "ResponseMessage", null, done);
+            });
+        });
+    });
+    describe('WriteAndEnd', () => {
+        test('HttpPostRequestSmallWriteSmallEnd', (done) => {
+            sendAndReceive(smallMessage, smallMessage, "ResponseMessage", null, done);
+        });
+        test('HttpPostRequestSmallWrite64kbEnd', (done) => {
+            sendAndReceive(smallMessage, exactly64kbMessage, "ResponseMessage", null, done);
+        });
+        test('HttpPostRequestLargeWriteSmallEnd', (done) => {
+            sendAndReceive(over64kbMessage, smallMessage, "ResponseMessage", null, done);
+        });
+    });
+    describe('MultipleWrites', () => {
+        test('HttpPostRequestSmallWrites', (done) => {
+            sendAndReceive([smallMessage, smallMessage], null, "ResponseMessage", null, done);
+        });
+        test('HttpPostRequestExceed64kbWrites', (done) => {
+            sendAndReceive([smallMessage, exactly64kbMessage], null, "ResponseMessage", null, done);
+        });
+        test('HttpPostRequestLargeThenSmallWrites', (done) => {
+            sendAndReceive([over64kbMessage, smallMessage], null, "ResponseMessage", null, done);
+        });
+    });
 });
 
-test('HttpPostLargeReqSmallRes', (done) => {
-    sendAndReceive(largeMessage, "Goodbye", done);
-});
-
-test('HttpPostLargeReqLargeRes', (done) => {
-    sendAndReceive(largeMessage, largeMessage, done);
+describe('HttpPostResponseTests', () => {
+    Object.keys(testMessages).forEach((testName) => {
+        test('HttpPostResponseEndOnly' + testName, (done) => {
+            sendAndReceive(null, "RequestMessage", null, testMessages[testName], done);
+        });
+    });
+    Object.keys(testMessages).forEach((testName) => {
+        test('HttpPostResponseWriteOnly' + testName, (done) => {
+            sendAndReceive("RequestMessage", null, testMessages[testName], null, done);
+        });
+    });
+    describe('WriteAndEnd', () => {
+        test('HttpPostResponseSmallWriteSmallEnd', (done) => {
+            sendAndReceive("RequestMessage", null, smallMessage, smallMessage, done);
+        });
+        test('HttpPostResponseSmallWrite64kbEnd', (done) => {
+            sendAndReceive("RequestMessage", null, smallMessage, exactly64kbMessage, done);
+        });
+        test('HttpPostResponseLargeWriteSmallEnd', (done) => {
+            sendAndReceive("RequestMessage", null, over64kbMessage, smallMessage, done);
+        });
+    });
+    describe('MultipleWrites', () => {
+        test('HttpPostResponseSmallWrites', (done) => {
+            sendAndReceive("RequestMessage", null, [smallMessage, smallMessage], null, done);
+        });
+        test('HttpPostResponseExceed64kbWrites', (done) => {
+            sendAndReceive("RequestMessage", null, [smallMessage, exactly64kbMessage], null, done);
+        });
+        test('HttpPostResponseLargeThenSmallWrites', (done) => {
+            sendAndReceive("RequestMessage", null, [over64kbMessage, smallMessage], null, done);
+        });
+    });
 });
